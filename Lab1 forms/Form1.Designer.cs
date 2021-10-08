5346bd608b55c46040f3b28deae459462d4f7b48
Алексей Халманов <khalmanov2003@mail.ru>
2021-10-08 14:10:19 +0800

    Добавил автотесты

--- Lab1 forms/Form1.Designer.cs
+++ Lab1 forms/Form1.Designer.cs
@@ -39,17 +39,17 @@ namespace Lab1_forms
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Location = new System.Drawing.Point(35, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(209, 19);
+            this.label1.Size = new System.Drawing.Size(153, 38);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Введите число от 1 до 9999:";
+            this.label1.Text = "Введите число \r\nкопеек от 1 до 9999:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(208, 62);
+            this.button1.Location = new System.Drawing.Point(35, 72);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.Size = new System.Drawing.Size(276, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Рассчитать";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,19 +57,22 @@ namespace Lab1_forms
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(227, 21);
+            this.textBox1.Location = new System.Drawing.Point(194, 33);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(88, 23);
+            this.textBox1.Size = new System.Drawing.Size(117, 23);
             this.textBox1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 132);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(336, 114);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
---
+++ Lab1 forms/Logic.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_forms
+{
+    public class Logic
+    {
+        public static string getresult(int n)
+        {
+            int ruble, penny, a, b;
+            string result = "";
+            if (1 <= n && n <= 9999)
+            {
+                ruble = n / 100;
+                penny = n % 100;
+                //проверка на однозначные числа рубли
+                if (ruble < 20)
+                {
+                    a = ruble;
+                }
+                else
+                {
+                    a = ruble % 20;
+                }
+                //проверка на однозначные числа копейки
+                if (penny < 20)
+                {
+                    b = penny;
+                }
+                else
+                {
+                    b = penny % 20;
+                }
+                //вывод: рублей
+                if (5 <= a && a <= 20 || a == 0)
+                {
+                    //вывод: копеек
+                    if (5 <= b && b <= 20 || b == 0)
+                    {
+                        result = ruble + " рублей " + penny + " копеек";
+                    }
+                    //вывод: копейка
+                    else if (b == 1)
+                    {
+                        result = ruble + " рублей " + penny + " копейка";
+                    }
+                    //вывод: копейки
+                    else
+                    {
+                        result = ruble + " рублей " + penny + " копейки";
+                    }
+                }
+                //вывод: рубль
+                else if (a == 1)
+                {
+                    //вывод: копеек
+                    if (5 <= b && b <= 20 || b == 0)
+                    {
+                        result = ruble + " рубль " + penny + " копеек";
+                    }
+                    //вывод: копейка
+                    else if (b == 1)
+                    {
+                        result = ruble + " рубль " + penny + " копейка";
+                    }
+                    //вывод: копейки
+                    else
+                    {
+                        result = ruble + " рубль " + penny + " копейки";
+                    }
+                }
+                //вывод: рубля
+                else
+                {
+                    //вывод: копеек
+                    if (5 <= b && b <= 20 || b == 0)
+                    {
+                        result = ruble + " рубля " + penny + " копеек";
+                    }
+                    //вывод: копейка
+                    else if (b == 1)
+                    {
+                        result = ruble + " рубля " + penny + " копейка";
+                    }
+                    //вывод: копейки
+                    else
+                    {
+                        result = ruble + " рубля " + penny + " копейки";
+                    }
+                }
+            }
+            else
+            {
+                result = "Число должно входить в промежуток от 1 до 9999!";
+            }
+            return result;
+        }
+    
+}
+}
